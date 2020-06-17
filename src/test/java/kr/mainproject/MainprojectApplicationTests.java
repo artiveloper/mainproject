9b31b1cef59956713318ad129824b87cb000780e
ravi <artiveloper@gmail.com>
2020-06-17 16:48:18 +0900

    first commit

---
+++ src/test/java/kr/mainproject/MainprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.mainproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
